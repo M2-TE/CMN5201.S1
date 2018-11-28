43423a79fe6b5d86f3e7b7e0edbc675004dada5f
Jan <ku-j@web.de>
2018-11-28 17:59:57 +0100

    Solo (Single Fire/Loop) Effects

---
+++ AbschlussProjekt/Assets/Code/Effects/BaseEffect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class BaseEffect : MonoBehaviour {
+    [SerializeField] public Sprite[] sprites;
+    [SerializeField] protected int frameRateOverride;
+    [SerializeField] protected bool looping;
+
+    protected SpriteRenderer ownSpriteRenderer;
+    protected float timeBetweenFrames;
+    protected int currentFrame;
+
+	protected IEnumerator Start ()
+    {
+        ownSpriteRenderer = GetComponent<SpriteRenderer>();
+        timeBetweenFrames = 1f / ((frameRateOverride != 0) ? frameRateOverride : sprites.Length);
+        currentFrame = -1;
+
+        WaitForSeconds waitTime = new WaitForSeconds(timeBetweenFrames);
+        while (true)
+        {
+            if (currentFrame < sprites.Length - 1) currentFrame++;
+            else if (looping) currentFrame = 0;
+            else break;
+
+            SetFrame();
+            yield return new WaitForSeconds(timeBetweenFrames);
+        }
+        Destroy(gameObject);
+	}
+
+    protected virtual void SetFrame()
+    {
+        ownSpriteRenderer.sprite = sprites[currentFrame];
+    }
+}
