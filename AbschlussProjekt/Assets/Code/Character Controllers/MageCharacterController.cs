5efb6b063d00dcc20bfbc8bed4f71d72ad467860
Jan <33833587+Model2TypeE@users.noreply.github.com>
2018-11-28 02:01:01 +0100

    Base Character Controller, Mage Character Controller (#23)
    -> Base CC with some virtual methods for extension to more unique characters

---
+++ AbschlussProjekt/Assets/Code/Character Controllers/MageCharacterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageCharacterController : BaseCharacterController
+{
+    protected override void CheckRequestedActions()
+    {
+        base.CheckRequestedActions();
+        TryMageBuff();
+    }
+
+    private void TryMageBuff()
+    {
+        if (Input.GetAxis("Fire2") > 0)
+        {
+            commandDelayCounter = attackDelay;
+            ownAnimator.SetFloat("Movespeed", 0f);
+            ownAnimator.SetTrigger("Buff");
+        }
+    }
+}
+
