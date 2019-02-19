a011b33d2cf74989b47f44684fb90384ca3cacaa
YDenker <33430355+YDenker@users.noreply.github.com>
2019-02-19 17:07:16 +0100

    Campfire done

---
+++ AbschlussProjekt/Assets/Code/FX/Disco/DiscoBall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class DiscoBall : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    public float SizeChange, ChangeSpeed;
+
+    public void OnEnable()
+    {
+        
+    }
+}
